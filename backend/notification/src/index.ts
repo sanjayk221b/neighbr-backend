88d72e77c60e961910e23891b1ff66fb10116e15
Sanjay <sanjaykeloth321@gmail.com>
2024-07-25 10:01:29 +0530

    refactor(notification): move index file to src

---
+++ backend/notification/src/index.ts
@@ -0,0 +1,42 @@
+import express, { Application } from "express";
+import { config } from "dotenv";
+import {
+  connectResidentCreatedConsumer,
+  disconnectResidentCreatedConsumer,
+} from "./events/kafka/consumers/resident-created.consumer";
+import {
+  connectCaretakerCreatedConsumer,
+  disconnectCaretakerCreatedConsumer,
+} from "./events/kafka/consumers/caretaker-created.consumer";
+
+config();
+const app: Application = express();
+const PORT: number = Number(process.env.PORT);
+
+const startServer = async () => {
+  try {
+    await connectResidentCreatedConsumer();
+    await connectCaretakerCreatedConsumer();
+    console.log("Kafka consumer connected");
+
+    app.listen(PORT, () => {
+      console.log(`Notification Service is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+  }
+};
+
+startServer();
+
+process.on("SIGINT", async () => {
+  try {
+    await disconnectResidentCreatedConsumer();
+    await disconnectCaretakerCreatedConsumer();
+    console.log("Kafka consumer disconnected");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error during shutdown:", error);
+    process.exit(1);
+  }
+});
