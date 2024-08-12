1d88d4904035709489de16b293ebe3684fe760a0
Sanjay <sanjaykeloth321@gmail.com>
2024-08-12 23:57:31 +0530

    delete: clean up unwanted files and move middleware to common repository

--- backend/management/src/infrastructure/repositories/interfaces/services.repository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { IService } from "@/entities";
-
-export interface IServiceRepository {
-  createService(service: IService, residentId: string): Promise<IService>;
-  getRequests(): Promise<IService[]>;
-  getRequestsByResidentId(residentId: string): Promise<IService[]>;
-  updateServiceRequests(serviceId: string, updateData: Partial<IService>): Promise<IService>;
-}
